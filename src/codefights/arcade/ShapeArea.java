cf7e933d9e5112f88b6caec6ee24ebbfd6dbc8ed
Sevinj Azizli <azizli.sevinc@gmail.com>
2018-04-23 23:41:45 +0200

    Adjusted the package name.

---
+++ src/codefights/arcade/ShapeArea.java
@@ -0,0 +1,25 @@
+package codefights.arcade;
+
+public class ShapeArea {
+
+	public static void main(String[] args) {
+		
+		shapeArea(5);
+	}
+
+	static int shapeArea(int n) {
+
+		if(n ==1)  return 1;
+			
+		int temp = (int) Math.pow(n, 2) + (int) Math.pow((n-1), 2);
+
+		System.out.println("The sheap area of "+n+" is "+temp);	
+			
+	
+	return temp;
+			
+}
+	
+	
+	
+}
